7e033245cdd1f4a2851e27684546238a2608b7f8
Andrei Mihu <andrei@heroiclabs.com>
2019-09-03 19:06:54 +0100

    Match-based party system.

---
+++ online_party_system/party.go
@@ -0,0 +1,44 @@
+// Copyright 2019 The Nakama Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package online_party_system
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/heroiclabs/nakama/runtime"
+)
+
+const (
+	serviceName = "OnlinePartySystem"
+)
+
+// Registers the collection of functions with Nakama required to provide an OnlinePartyService from Unreal Engine.
+func Register(initializer runtime.Initializer) error {
+	if err := initializer.RegisterMatch(fmt.Sprintf("%s-%s", serviceName, "Party"), createPartyMatch); err != nil {
+		return err
+	}
+	if err := initializer.RegisterRpc(fmt.Sprintf("%s-%s", serviceName, "CreateParty"), rpcCreateParty); err != nil {
+		return err
+	}
+	if err := initializer.RegisterRpc(fmt.Sprintf("%s-%s", serviceName, "GetAdvertisedParty"), rpcGetAdvertisedParty); err != nil {
+		return err
+	}
+	return nil
+}
+
+func createPartyMatch(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule) (runtime.Match, error) {
+	return &PartyMatch{}, nil
+}
---
+++ online_party_system/party_rpc.go
@@ -0,0 +1,197 @@
+// Copyright 2019 The Nakama Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package online_party_system
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"github.com/heroiclabs/nakama/runtime"
+)
+
+var (
+	ErrBadContext    = runtime.NewError("bad context", 3)
+	ErrJsonMarshal   = runtime.NewError("cannot marshal response", 13)
+	ErrJsonUnmarshal = runtime.NewError("cannot unmarshal request", 13)
+)
+
+type CreatePartyCompletionResult int
+
+const (
+	CreatePartyCompletionResult_UnknownClientFailure          CreatePartyCompletionResult = -100
+	CreatePartyCompletionResult_AlreadyInPartyOfSpecifiedType CreatePartyCompletionResult = -99
+	CreatePartyCompletionResult_AlreadyCreatingParty          CreatePartyCompletionResult = -98
+	CreatePartyCompletionResult_AlreadyInParty                CreatePartyCompletionResult = -97
+	CreatePartyCompletionResult_FailedToCreateMucRoom         CreatePartyCompletionResult = -96
+	CreatePartyCompletionResult_NoResponse                    CreatePartyCompletionResult = -95
+	CreatePartyCompletionResult_LoggedOut                     CreatePartyCompletionResult = -94
+	CreatePartyCompletionResult_UnknownInternalFailure        CreatePartyCompletionResult = 0
+	CreatePartyCompletionResult_Succeeded                     CreatePartyCompletionResult = 1
+)
+
+type CreatePartyRequest struct {
+	PartyTypeId        int64  `json:"party_type_id"`
+	PartyConfiguration string `json:"party_configuration"`
+}
+
+type CreatePartyResponse struct {
+	PartyId                     string                      `json:"party_id"`
+	CreatePartyCompletionResult CreatePartyCompletionResult `json:"create_party_completion_result"`
+}
+
+func rpcCreateParty(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	var session *SessionContext
+	var err error
+	if session, err = unpackContext(ctx); err != nil {
+		logger.Error("unpack context failed: %v", err)
+		return "", ErrBadContext
+	}
+
+	var request *CreatePartyRequest
+	if err = json.Unmarshal([]byte(payload), &request); err != nil {
+		return "", ErrJsonUnmarshal
+	}
+
+	// Validate input. Online party type ID must be an integer >= 0.
+	if request.PartyTypeId < 0 {
+		response := &CreatePartyResponse{
+			CreatePartyCompletionResult: CreatePartyCompletionResult_FailedToCreateMucRoom,
+		}
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			return "", ErrJsonMarshal
+		}
+		return string(responseBytes), nil
+	}
+	// Validate input. Party configuration must be a string <= 1024 bytes long.
+	if len(request.PartyConfiguration) > 1024 {
+		response := &CreatePartyResponse{
+			CreatePartyCompletionResult: CreatePartyCompletionResult_FailedToCreateMucRoom,
+		}
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			return "", ErrJsonMarshal
+		}
+		return string(responseBytes), nil
+	}
+
+	// Create the party match.
+	matchId, err := nk.MatchCreate(ctx, fmt.Sprintf("%s-%s", serviceName, "Party"), map[string]interface{}{
+		"creator": session.UserID,
+		"type_id": request.PartyTypeId,
+		"config":  request.PartyConfiguration,
+	})
+	if err != nil {
+		response := &CreatePartyResponse{
+			CreatePartyCompletionResult: CreatePartyCompletionResult_UnknownInternalFailure,
+		}
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			return "", ErrJsonMarshal
+		}
+		return string(responseBytes), nil
+	}
+
+	// Return result to user.
+	response := &CreatePartyResponse{
+		PartyId:                     matchId,
+		CreatePartyCompletionResult: CreatePartyCompletionResult_Succeeded,
+	}
+	responseBytes, err := json.Marshal(response)
+	if err != nil {
+		return "", ErrJsonMarshal
+	}
+	return string(responseBytes), nil
+}
+
+type GetAdvertisedPartyRequest struct {
+	UserId      string `json:"user_id"`
+	PartyTypeId int64  `json:"party_type_id"`
+}
+
+type GetAdvertisedPartyResponse struct {
+	PartyId string `json:"party_id"`
+}
+
+func rpcGetAdvertisedParty(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	var err error
+	if _, err = unpackContext(ctx); err != nil {
+		logger.Error("unpack context failed: %v", err)
+		return "", ErrBadContext
+	}
+
+	var request *GetAdvertisedPartyRequest
+	if err = json.Unmarshal([]byte(payload), &request); err != nil {
+		return "", ErrJsonUnmarshal
+	}
+
+	// Validate input. Online party type ID must be an integer >= 0.
+	if request.PartyTypeId < 0 {
+		// Not an error necessarily, rather no possible matches.
+		response := &GetAdvertisedPartyResponse{}
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			return "", ErrJsonMarshal
+		}
+		return string(responseBytes), nil
+	}
+	// Validate input. A non-empty user ID is required.
+	if len(request.UserId) == 0 {
+		// Not an error necessarily, rather no possible matches.
+		response := &GetAdvertisedPartyResponse{}
+		responseBytes, err := json.Marshal(response)
+		if err != nil {
+			return "", ErrJsonMarshal
+		}
+		return string(responseBytes), nil
+	}
+
+	// List matches looking for the appropriate one.
+	query := fmt.Sprintf("+label.type_id:%v +label.members:%v", request.PartyTypeId, request.UserId)
+	matches, err := nk.MatchList(ctx, 1, true, "", 0, 0, query)
+	if err != nil {
+		logger.Error("match list failed: %v", err)
+		return "", ErrBadContext
+	}
+
+	// Return result to user.
+	response := &GetAdvertisedPartyResponse{}
+	if len(matches) >= 1 {
+		response.PartyId = matches[0].MatchId
+	}
+	responseBytes, err := json.Marshal(response)
+	if err != nil {
+		return "", ErrJsonMarshal
+	}
+	return string(responseBytes), nil
+}
+
+type SessionContext struct {
+	UserID    string
+	SessionID string
+}
+
+func unpackContext(ctx context.Context) (*SessionContext, error) {
+	userId, ok := ctx.Value(runtime.RUNTIME_CTX_USER_ID).(string)
+	if !ok {
+		return nil, ErrBadContext
+	}
+	sessionId, ok := ctx.Value(runtime.RUNTIME_CTX_SESSION_ID).(string)
+	if !ok {
+		return nil, ErrBadContext
+	}
+	return &SessionContext{UserID: userId, SessionID: sessionId}, nil
+}
