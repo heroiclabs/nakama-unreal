ac29ec280a434309fa57dd84bade7a336c3ad167
Luke Gehorsam <luke@heroiclabs.com>
2022-09-04 13:35:56 -0400

    add unreal helpers to NakamaUnreal.h

--- Nakama/Source/Public/NakamaUnreal.h
+++ Nakama/Source/Public/NakamaUnreal.h
@@ -1,6 +1,20 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include <nakama-cpp/Nakama.h>
+
+NAKAMA_NAMESPACE_BEGIN
+
+namespace Unreal {
+
+void initLogging(NLogLevel level);
+NClientPtr createNakamaClient(const NClientParameters& parameters, NLogLevel level);
+NRtClientPtr createNakamaRtClient(const NClientPtr& client, int32_t port);
+NRtClientPtr createNakamaRtClient(const NClientPtr& client, const RtClientParameters& params);
+
+}
+
+NAKAMA_NAMESPACE_END
 
 class FNakamaModule : public IModuleInterface
 {
@@ -9,3 +23,5 @@ public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 };
+
+#pragma once
