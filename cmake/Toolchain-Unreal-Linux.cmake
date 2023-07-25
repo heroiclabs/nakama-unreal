4e78c24c52ee8a9e1e3e8a5b2b56c02286a17d6a
Luke Gehorsam <lugehorsam@gmail.com>
2023-07-25 13:08:45 -0400

    Remove duplication libc++

--- cmake/Toolchain-Unreal-Linux.cmake
+++ cmake/Toolchain-Unreal-Linux.cmake
@@ -5,4 +5,4 @@
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
 set(CMAKE_CXX_FLAGS "-nostdinc++ -nodefaultlibs -I$ENV{UNREAL_ENGINE}/Engine/Source/ThirdParty/Unix/LibCxx/include/ -I$ENV{UNREAL_ENGINE}/Engine/Source/ThirdParty/Unix/LibCxx/include/c++/v1 ${CMAKE_CXX_FLAGS}")
-set(CMAKE_CXX_STANDARD_LIBRARIES "-lpthread -lm -lc -lgcc_s -lgcc $ENV{UNREAL_ENGINE}/Engine/Source/ThirdParty/Unix/LibCxx/lib/Unix/x86_64-unknown-linux-gnu/libc++.a $ENV{UNREAL_ENGINE}/Engine/Source/ThirdParty/Unix/LibCxx/lib/Unix/x86_64-unknown-linux-gnu/libc++.a")
+set(CMAKE_CXX_STANDARD_LIBRARIES "-lpthread -lm -lc -lgcc_s -lgcc $ENV{UNREAL_ENGINE}/Engine/Source/ThirdParty/Unix/LibCxx/lib/Unix/x86_64-unknown-linux-gnu/libc++.a)
