42f6dc1fb99052be8aeeb683a889c018222cdba6
Luke Gehorsam <luke@heroiclabs.com>
2023-10-30 11:44:58 -0400

    move breset to query param and use account object properly

--- Nakama/Source/NakamaUnreal/Private/NakamaClient.cpp
+++ Nakama/Source/NakamaUnreal/Private/NakamaClient.cpp
@@ -4747,17 +4747,17 @@ void UNakamaClient::ImportSteamFriends(
         return;
     }
 
-    // Setup the request content
-    const TSharedPtr<FJsonObject> AccountJson = MakeShared<FJsonObject>();
-    AccountJson->SetStringField(TEXT("token"), SteamToken);
-
-    const TSharedPtr<FJsonObject> ContentJson = MakeShared<FJsonObject>();
-    ContentJson->SetObjectField(TEXT("account"), AccountJson);
+    // Setup the query parameters
+    TMultiMap<FString, FString> QueryParams;
 
     if (bReset.IsSet())
     {
-        ContentJson->SetBoolField(TEXT("reset"), bReset.GetValue());
+        QueryParams.Add(TEXT("reset"), bReset.GetValue() ? TEXT("true") : TEXT("false"));
     }
+
+    // Setup the request content
+    const TSharedPtr<FJsonObject> ContentJson = MakeShared<FJsonObject>();
+    ContentJson->SetStringField(TEXT("token"), SteamToken);
 
     // Serialize the request content
     FString Content;
@@ -4769,7 +4769,7 @@ void UNakamaClient::ImportSteamFriends(
     }
 
     // Make the request
-    const auto HttpRequest = MakeRequest(Endpoint, Content, ENakamaRequestMethod::POST, TMultiMap<FString, FString>(), Session->GetAuthToken());
+    const auto HttpRequest = MakeRequest(Endpoint, Content, ENakamaRequestMethod::POST, QueryParams, Session->GetAuthToken());
 
     // Lock the ActiveRequests mutex to protect concurrent access
     FScopeLock Lock(&ActiveRequestsMutex);
