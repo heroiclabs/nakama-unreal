ba599b3a94bd1d6927e9bd102cb5e404a46b6102
Ricard Rovira Cubeles <rrctrashbin@gmail.com>
2024-11-08 00:04:38 +0100

    Update default config values due to deprecation
    (cherry picked from commit e0e605c853fe69a4c87dba917f8191b1676c1e68)

--- NakamaTest/Source/NakamaTest.Target.cs
+++ NakamaTest/Source/NakamaTest.Target.cs
@@ -8,7 +8,7 @@ public class NakamaTestTarget : TargetRules
 	public NakamaTestTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "NakamaTest" } );
 		bUseLoggingInShipping = true;
 	}
--- NakamaTest/Source/NakamaTestEditor.Target.cs
+++ NakamaTest/Source/NakamaTestEditor.Target.cs
@@ -8,7 +8,7 @@ public class NakamaTestEditorTarget : TargetRules
        public NakamaTestEditorTarget( TargetInfo Target) : base(Target)
        {
                Type = TargetType.Editor;
-               DefaultBuildSettings = BuildSettingsVersion.V2;
+               DefaultBuildSettings = BuildSettingsVersion.V5;
                ExtraModuleNames.AddRange( new string[] { "NakamaTest" } );
        }
 }
