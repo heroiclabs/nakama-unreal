e015185685d85487bbd58593a1781f00efbeb05e
Ricard Rovira Cubeles <rrctrashbin@gmail.com>
2025-08-17 20:44:38 +0200

    Add some basic tests

--- NakamaBlueprintsTest/Source/NakamaBlueprintsTestEditor.Target.cs
+++ NakamaBlueprintsTest/Source/NakamaBlueprintsTestEditor.Target.cs
@@ -8,7 +8,7 @@ public class NakamaBlueprintsTestEditorTarget : TargetRules
 	public NakamaBlueprintsTestEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "NakamaBlueprintsTest" } );
 	}
