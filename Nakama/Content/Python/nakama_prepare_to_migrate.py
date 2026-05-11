5ab66199eef8d591f8bcbf79af75b57f0185a3a6
Seva <seva@heroiclabs.com>
2026-05-11 11:24:18 +0200

    [etc] Adjust copyright years

--- Nakama/Content/Python/nakama_prepare_to_migrate.py
+++ Nakama/Content/Python/nakama_prepare_to_migrate.py
@@ -1,5 +1,5 @@
 """
-Copyright 2025 The Nakama Authors
+Copyright 2026 The Nakama Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
