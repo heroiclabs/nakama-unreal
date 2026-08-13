50e6794725c58ab305e03085fc45fe94a3235f41
Seva <seva@heroiclabs.com>
2026-08-13 17:46:07 +0200

    [satori] Fix for FString->FSharedString move, replace .Add with .Emplace.

--- Satori/Source/SatoriUnreal/Private/SatoriProperties.cpp
+++ Satori/Source/SatoriUnreal/Private/SatoriProperties.cpp
@@ -25,26 +25,29 @@ FSatoriProperties::FSatoriProperties(const TSharedPtr<FJsonObject> JsonObject)
 	if (JsonObject.IsValid())
 	{
 		const TSharedPtr<FJsonObject>* MetadataObject = nullptr;
-		if (JsonObject->TryGetObjectField(TEXT("default"), MetadataObject)) {
-			for (const TPair<FString, TSharedPtr<FJsonValue>>& Pair : (*MetadataObject)->Values)
+		if (JsonObject->TryGetObjectField(TEXT("default"), MetadataObject))
+		{
+			for (const TPair<UE::FSharedString, TSharedPtr<FJsonValue>>& Pair : (*MetadataObject)->Values)
 			{
-				DefaultProperties.Add(*Pair.Key, Pair.Value->AsString());
+				DefaultProperties.Emplace(*Pair.Key, Pair.Value->AsString());
 			}
 		}
 
 		MetadataObject = nullptr;
-		if (JsonObject->TryGetObjectField(TEXT("computed"), MetadataObject)) {
-			for (const TPair<FString, TSharedPtr<FJsonValue>>& Pair : (*MetadataObject)->Values)
+		if (JsonObject->TryGetObjectField(TEXT("computed"), MetadataObject))
+		{
+			for (const TPair<UE::FSharedString, TSharedPtr<FJsonValue>>& Pair : (*MetadataObject)->Values)
 			{
-				ComputedProperties.Add(*Pair.Key, Pair.Value->AsString());
+				ComputedProperties.Emplace(*Pair.Key, Pair.Value->AsString());
 			}
 		}
 
 		MetadataObject = nullptr;
-		if (JsonObject->TryGetObjectField(TEXT("custom"), MetadataObject)) {
-			for (const TPair<FString, TSharedPtr<FJsonValue>>& Pair : (*MetadataObject)->Values)
+		if (JsonObject->TryGetObjectField(TEXT("custom"), MetadataObject))
+		{
+			for (const TPair<UE::FSharedString, TSharedPtr<FJsonValue>>& Pair : (*MetadataObject)->Values)
 			{
-				CustomProperties.Add(*Pair.Key, Pair.Value->AsString());
+				CustomProperties.Emplace(*Pair.Key, Pair.Value->AsString());
 			}
 		}
 	}
